b71e09cb983df5353f92cf124ee88ce98810bac1
brunstighanhval <mlkaer1@hotmail.com>
2023-04-27 10:59:14 +0200

    hjbknl

--- src/main/java/BLL/CustomerManager.java
+++ src/main/java/BLL/CustomerManager.java
@@ -1,4 +1,26 @@
+/*
 package BLL;
 
+import java.io.IOException;
+import java.util.List;
 public class CustomerManager {
+
+    private main.java.DAL.ICustomerDataAccess customerDAO;
+
+    public CustomerManager() throws IOException {customerDAO = new main.java.DAL.CustomerDAO();}
+
+    public List<main.java.BE.Customer> loadProjectManager() throws Exception{return customerDAO.loadProjectManager();}
+
+    public main.java.BE.Customer createNewCustomer(String address, int phonenumber, String companyname, int zipcode, String mail, String firstName, String lastName, int room) throws Exception {return  customerDAO.createNewCustomer(address, phonenumber, companyname, zipcode, mail, firstName, lastName, room);}
+
+    public void deleteCustomer(main.java.BE.Customer selectedCustomer) throws Exception {customerDAO.delete(selectedCustomer);}
+
+
+
+
+
+
 }
+
+ */
+
---
+++ src/main/java/DAL/ICustomerDataAccess.java
@@ -0,0 +1,16 @@
+/*
+package DAL;
+
+import main.java.BE.Customer;
+import java.util.List;
+
+public interface ICustomerDataAccess {
+
+    List<Customer> loadCustomer(String companyname, int phonenumber, String address, int room);
+
+
+}
+
+ */
+
+
